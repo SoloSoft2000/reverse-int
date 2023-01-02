65ea50d7a92560b1a6f46b79d5df075ac45e2e0d
SoloSoft2000 <solosoft2000@mail.ru>
2023-01-02 15:58:40 +0200

    feat: release function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const str = Math.abs(n).toString(10);
+  newStr = "";
+  for (i = str.length-1; i >= 0; i--) {
+    if(str!="-") {
+        newStr += str[i];
+    }
+  }
+  myNum = Number(newStr);
+  return(myNum);
 }
